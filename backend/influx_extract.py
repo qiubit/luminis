2b5a943b367c93d7dbe0e2a1ceacf09029ad3251
rm360179 <rm360179@gmail.com>
2016-12-02 15:49:16 +0100

    small fixes, example of use

--- backend/influx_extract.py
+++ backend/influx_extract.py
@@ -5,12 +5,8 @@ class InfluxQuery:
     #@TODO doesnt' show tags, will have to write own queries parsing insetead of idb
     def query(self, measurement, tag_name, tag_value):
         # group by is due to changes in influx and problems with python-influxdb
-        #@TODO SQL injection there
         query_statement='SELECT * FROM ' + measurement + ' GROUP BY *'
         return (list(self.client.query(query_statement).get_points(tags={tag_name: tag_value})))
-        
-
-
 
 class InfluxReader:
     def __init__(self,
@@ -23,7 +19,6 @@ class InfluxReader:
         self.port=port
         self.database=database
         self.username=username
-        # password like that?!
         self.password=password
         self.client=idb.InfluxDBClient(host=host, port=port, database=database, username=username, password=password)
     def databases(self):
@@ -32,6 +27,7 @@ class InfluxReader:
         iq=InfluxQuery(self.client)
         return iq.query(measurement=measurement, tag_name=tag_name, tag_value=tag_value)
 
-writer = InfluxReader()
-#client=idb.InfluxDBClient(host='localhost', port=8086, username=username, password=password)
-print(writer.query(measurement='car', tag_name='size', tag_value='12'))
+
+# example use:
+# reader = InfluxReader()
+# print(reader.query(measurement='car', tag_name='size', tag_value='12'))
