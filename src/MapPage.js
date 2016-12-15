be658acf7b5ffdadcb878ee37fa7d6b48eb437b2
Piotr Wisniewski <pw361286@students.mimuw.edu.pl>
2016-12-15 21:33:46 +0100

    (25) Mapa + wyświetlanie drzewa

--- src/MapPage.js
+++ src/MapPage.js
@@ -1,4 +1,8 @@
 import React from 'react';
+import { Map, Marker, Popup, TileLayer } from 'react-leaflet';
+import config from './config';
+import sample_tree from './sample_tree'
+
 
 const styles = {
   container: {
@@ -10,16 +14,68 @@ const styles = {
 };
 
 class MapPage extends React.Component {
+
+  mapTreeToMarkers = (tree) => {
+    if (tree){
+      return (
+          <div>
+          <Marker key={tree[0].id} position={tree[0].position}>
+            <Popup>
+              <span>Grouping node<br/>id: {tree[0].id}</span>
+            </Popup>
+          </Marker>
+          {
+            tree[0].children.map(node => (
+              <Marker key={node.id} position={node.position}>
+                <Popup>
+                  <span>Child node<br/>id: {node.id}.</span>
+                </Popup>
+              </Marker>
+            ))
+          }
+          </div>
+      );
+    }
+  }
+
+  getBounds = (tree) => {
+    var upperLeft = [1, 1];
+    var lowerRight = [0, 0];
+
+    if (tree) {
+      upperLeft[0] = tree[0].position[0];
+      upperLeft[1] = tree[0].position[1];
+      lowerRight[0] = tree[0].position[0];
+      lowerRight[1] = tree[0].position[1];
+    }
+
+    for (var i = 0; i < tree[0].children.length; ++i) {
+      upperLeft[0] = Math.max(upperLeft[0], tree[0].children[i].position[0]);
+      upperLeft[1] = Math.min(upperLeft[1], tree[0].children[i].position[1]);
+      lowerRight[0] = Math.min(lowerRight[0], tree[0].children[i].position[0]);
+      lowerRight[1] = Math.max(lowerRight[1], tree[0].children[i].position[1]);
+    }
+
+    return [upperLeft, lowerRight];
+  }
+
   render() {
     return (
-      <div>
-        <div style={styles.container}>
-          <h1>This is map placeholder</h1>
-        </div>
-        <div style={styles.container}>
-          <h1>This is chart placeholder</h1>
-        </div>
-      </div>
+      <Map
+        center={this.props.position}
+        style={{height: "300px"}}
+        bounds={this.getBounds(this.props.tree)}
+        boundsOptions={{padding: config.mapPadding}}
+      >
+        <TileLayer
+          url={'https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}'}
+          attribution={'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>'}
+          maxZoom={config.mapMaxZoom}
+          id={config.mapTileLayerId}
+          accessToken={config.openStreetMapAccessToken}
+        />
+        {this.mapTreeToMarkers(this.props.tree)}
+      </Map>
     );
   }
 }
--- src/sample_tree.js
+++ src/sample_tree.js
@@ -1,22 +1,27 @@
 var tree = [{
     id: '1',
     primaryText: 'node 1',
+    position: [52.2088199, 20.997372],
     children: [
       {
         id: '2',
         primaryText: 'node 2',
+        position: [52.2078299, 20.997372],
         children: [
           {
             id: '4',
             primaryText: 'node 4',
+            position: [52.2068399, 20.997372],
             children: [
               {
                 id: '5',
                 primaryText: 'node 5',
+                position: [52.2058499, 20.997372],
                 children: [
                   {
                     id: '6',
                     primaryText: 'node 6',
+                    position: [52.2048599, 20.997372],
                     children: [],
                   }
                 ],
@@ -28,10 +33,10 @@ var tree = [{
       {
         id: '3',
         primaryText: 'node 3',
+        position: [52.2038699, 20.997372],
         children: []
       }
     ]
 }]
 
 export default tree;
-
