3b7dd86735f887bfbf39adb43ea17037257d2d71
Dominik Murzynowski <d.murzynowski@students.mimuw.edu.pl>
2016-12-23 13:16:38 +0100

    (22) Fixes after code review

--- backend/influx_selector.py
+++ backend/influx_selector.py
@@ -1,11 +1,7 @@
 import requests
-import influxdb
 
-class Error(Exception):
-    """Base class for exceptions in this module."""
-    pass
 
-class QueryError(Error):
+class QueryError(Exception):
     """Exception raised when in case of server error.
 
     Attributes:
@@ -13,71 +9,91 @@ class QueryError(Error):
         message -- explanation of the error
     """
 
-    def __init__(self, expression, message):
-        self.expression = expression
+    def __init__(self, code, message):
+        self.code = code
+        self.message = message
 
-class InfluxReader:
+    def __str__(self):
+        return "Query error {}: {}".format(self.code, self.message)
+
+
+class InfluxReader(object):
     """Used to create queries on one measurement
     """
     def __init__(self,
-            host='localhost',
-            port='8086',
-            username='root',
-            password='root',
-            database='mydb'):
+                 host='localhost',
+                 port='8086',
+                 username='root',
+                 password='root',
+                 database='mydb'):
         self._host = host
         self._port = port
         self._username = username
         self._password = password
         self._database = database
         self._session = requests.Session()
-    def query(self, measurement, attributes=('*',), where='', apply_cols=True):
+
+    def query(self, measurement, attributes=('*',), constraints=(), apply_cols=False):
         """
-        Query influxdb for data in json format
+        Query influxdb for data in json format.
+        Any constraints can be specified in format (<field>, [<operator>], <value>).
+        If operator is omitted, "=" is assumed.
 
         Args:
             measurement (str): measurement we take the data from (what comes after FROM)
             attributes (tuple/str): tuple or single string of cols we want to receive (what comes after SELECT)
-            where (str): where statement as in influxdb docs
+            constraints (iterable): Iterable of constraints
             apply_cols (bool): if set to True the function will return list of dicts in format:
                                [{<colname1>:<value1>, ...},{<colname1>:<value2>,...}, ...]
                                if set to False the function will return lists of columns names and record values
-                               independetly,
+                               independently,
         Returns: 
             str: records in json format depending on function arguments
         """
         if type(attributes) is not tuple:
-            attributes=(attributes,)
-        if where != '':
-            where = 'WHERE ' + where
-        params={}
+            attributes = (attributes,)
+        if constraints:
+            where_clause = 'WHERE '
+            constraint_strings = []
+            for constraint in constraints:
+                if len(constraint) == 2:  # implicit operator "="
+                    constraint_strings.append("{} = {}".format(*constraint))
+                else:
+                    constraint_strings.append("{} {} {}".format(*constraint))
+            where_clause += " and ".join(constraint_strings)
+        else:
+            where_clause = ''
+
+        params = dict()
         params['db'] = self._database
-        params['q'] = 'SELECT ' + ','.join(attributes) + ' FROM ' + measurement + ' ' + where
+        params['q'] = 'SELECT ' + ','.join(attributes) + ' FROM ' + measurement + ' ' + where_clause
         response = self._session.request(
-                method = "GET",
+                method="GET",
                 url='http://' + self._host + ':' + self._port + '/query',
                 auth=(self._username, self._password),
                 params=params)
 
-        if(response.status_code != 200):
+        if response.status_code != 200:
             raise QueryError(response.status_code, response.json().get('error', 'unknown'))
         result = response.json().get('results', [{}])[0].get('series', [[]])[0]
-        if(apply_cols):
+        if apply_cols:
             return self._apply_cols(result)
         else:
             return result
-    def _apply_cols(self, json):
-        if json == []:
+
+    @staticmethod
+    def _apply_cols(json):
+        if json:
             return []
         result = []
         cols = json['columns']
-        vals = json['values']
-        for record in vals:
+        values = json['values']
+        for record in values:
             result.append(dict(zip(cols, record)))
         return result
 
 '''
 # Example:
 ir = InfluxReader(host='localhost', port='8086', database='mydb')
-print(ir.query(attributes=('time', 'value'), measurement='cpu', where='value=0.63 or value=0.64'))
+print(ir.query(attributes=('time', 'value'), measurement='cpu', constraints=(('value', 0.63), ('time', '>', 'now() - 5m'))))
 '''
