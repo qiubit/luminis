bc9483c2331230d533f4d496911000c1b60ec43e
Dominik Murzynowski <d.murzynowski@students.mimuw.edu.pl>
2017-01-21 14:26:23 +0100

    (34) API server can be started locally

--- backend/api/app.py
+++ backend/api/app.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 from pycnic.core import WSGI
 from api.tree import *
 
@@ -9,3 +11,13 @@ class Application(WSGI):
         (r'/node', Node()),
         (r'/node/(\d+)', Node()),
     ]
+
+if __name__ == "__main__":
+    # this module can be executed to get simple WSGI server on localhost:8080
+    from wsgiref.simple_server import make_server
+    try:
+        print("Serving on 0.0.0.0:8080...")
+        make_server('0.0.0.0', 8080, Application).serve_forever()
+    except KeyboardInterrupt:
+        pass
+    print("Done")
