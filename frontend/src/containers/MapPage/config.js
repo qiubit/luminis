6e4900b6df10ff5e29568640085616d5983b92dd
Pawel Golinski <golpaw1@gmail.com>
2017-01-25 23:56:56 +0100

    Remove MapPage config.js

--- frontend/src/containers/MapPage/config.js
+++
@@ -1,8 +0,0 @@
-var config = {
-  openStreetMapAccessToken: 'pk.eyJ1Ijoid2lzbmlhazE5OSIsImEiOiJjaXY5c2EyZnMwMDJmMm9sNWV0YXloZXoyIn0.Q4f-N5IEUVF-YuIBxkt1JQ',
-  mapPadding: [50, 50],
-  mapMaxZoom: 18,
-  mapTileLayerId: 'mapbox.satellite',
-}
-
-export default config;
