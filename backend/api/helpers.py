6358624682933d338d60836e02ce15446001bad8
Dominik Murzynowski <github@domyno.pl>
2017-02-06 22:27:01 +0100

    (53) Bugfix

--- backend/api/helpers.py
+++ backend/api/helpers.py
@@ -2,10 +2,12 @@ from pycnic.errors import HTTP_404
 
 
 def get_all(session, cls):
-    return session.query(cls).filter(delete_ts=None).all()
+    return session.query(cls).filter_by(delete_ts=None).all()
 
 
 def get_one(session, cls, ident):
-    result = session.query(cls).filter(id=ident, delete_ts=None).all()
+    result = session.query(cls).filter_by(id=ident, delete_ts=None).all()
     if len(result) != 1:
         raise HTTP_404("{} with ID: {} not found".format(cls.__name__, ident))
+    else:
+        return result[0]
