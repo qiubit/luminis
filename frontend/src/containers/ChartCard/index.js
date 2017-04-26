b3c55b1532068c4a539238de63d8ab2e319d7227
Piotr Wisniewski <pw361286@students.mimuw.edu.pl>
2017-04-26 22:25:58 +0200

    Datetime picker for custom

--- frontend/src/containers/ChartCard/index.js
+++ frontend/src/containers/ChartCard/index.js
@@ -2,9 +2,10 @@ import React from 'react'
 import Dygraph from 'dygraphs'
 import { Map, fromJS } from 'immutable'
 import MomentDate from 'moment'
+import DateTimePicker from 'react-datetime'
 
 
-import { Card, CardTitle, CardText } from 'material-ui/Card'
+import { Card, CardTitle, CardText, Paper } from 'material-ui/Card'
 import RaisedButton from 'material-ui/RaisedButton'
 import 'dygraphs/dist/dygraph.css'
 
@@ -29,6 +30,44 @@ import {
   AGGREGATION_CUSTOM,
 } from './constants'
 
+
+class DatePickerBar extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      begin: props.begin,
+      end: props.end
+    }
+    this.setBegin = this.setBegin.bind(this)
+    this.setEnd = this.setEnd.bind(this)
+  }
+
+  setBegin(newBegin) {
+    if (!(typeof newBegin === 'string' || newBegin instanceof String)) {
+      this.setState({begin: newBegin})
+    }
+  }
+
+  setEnd(newEnd) {
+    if (!(typeof newEnd === 'string' || newEnd instanceof String)) {
+      this.setState({end: newEnd})
+    }
+  }
+
+  render() {
+    return (
+      <div>
+      <DateTimePicker onChange={this.setBegin} open={false} dateFormat={'DD.MM.YYYY'} timeFormat={'HH:mm:ss'} defaultValue={this.props.begin}/>
+      <RaisedButton
+        onClick={this.props.onSubmit(this.state.begin, this.state.end)}
+        primary={true}
+        label="SUBMIT"
+      />
+      <DateTimePicker onChange={this.setEnd} open={false} dateFormat={'DD.MM.YYYY'} timeFormat={'HH:mm:ss'} defaultValue={this.props.end}/>
+      </div>
+    )
+  }
+}
 
 class Graph extends React.Component {
 
@@ -83,7 +122,6 @@ class ChartCard extends React.Component {
     let begin = (new MomentDate()).subtract(1, 'day')
     let end = new MomentDate()
     let initDataRange = fromJS({
-      label: DATA_RANGE_LIVE,
       begin,
       end
     })
@@ -101,11 +139,14 @@ class ChartCard extends React.Component {
       data: [],
       labels: [],
       visibility: [],
+      showDatePickers: false,
+      activeDataRangeButton: DATA_RANGE_LIVE
     }
     this.onDataRangeChange = this.onDataRangeChange.bind(this)
     this.onAggregationChange = this.onAggregationChange.bind(this)
     this.updateSubscriptions = this.updateSubscriptions.bind(this)
     this.makeRequest = this.makeRequest.bind(this)
+    this.onSubmit = this.onSubmit.bind(this)
   }
 
 
@@ -156,7 +197,7 @@ class ChartCard extends React.Component {
     let updateData = false
     let aggregationLength = this.state.aggregation.get('value')
     let aggregationType = 'mean'
-    return requestNewChart(nodeId, requestedData, beginTs, endTs, updateData, aggregationLength, aggregationType)
+    return requestNewChart(parseInt(nodeId), requestedData, beginTs, endTs, updateData, aggregationLength, aggregationType)
   }
 
 
@@ -214,14 +255,25 @@ class ChartCard extends React.Component {
     this.updateSubscriptions(fromJS([]))
   }
 
-  onDataRangeChange = (label, begin, end) => () => {
-    let newDataRange = fromJS({
-      label,
-      begin,
-      end
-    })
-    this.setState({ dataRange: newDataRange })
+  onDataRangeChange = (button, begin, end, showDatePickers) => () => {
+    if (begin && end) {
+      let newDataRange = fromJS({
+        begin,
+        end,
+      })
+      this.setState({dataRange: newDataRange, activeDataRangeButton: button, showDatePickers})
+    } else {
+      this.setState({activeDataRangeButton: button, showDatePickers})
+    }
   }
+
+  onSubmit = (newBegin, newEnd) => () => {
+    let newDataRange = this.state.dataRange
+    newDataRange = newDataRange.set('begin', newBegin).set('end', newEnd)
+    this.setState({dataRange: newDataRange})
+  }
+
+
 
   onAggregationChange = (label, value) => () => {
     let newAggregation = fromJS({
@@ -239,30 +291,38 @@ class ChartCard extends React.Component {
         <CardText>
           <div>
             <Graph data={this.state.data} labels={this.state.labels} visibility={this.state.visibility}/>
+            {
+              this.state.showDatePickers &&
+                <DatePickerBar
+                  begin={this.state.dataRange.get('begin')}
+                  end={this.state.dataRange.get('end')}
+                  onSubmit={this.onSubmit}
+                />
+            }
             <p>Data Range</p>
             <RaisedButton
-              onClick={this.onDataRangeChange(DATA_RANGE_LIVE, (new MomentDate()).subtract(1, 'day'), new MomentDate())}
-              primary={this.state.dataRange.get('label') === DATA_RANGE_LIVE}
+              onClick={this.onDataRangeChange(DATA_RANGE_LIVE, (new MomentDate()).subtract(1, 'day'), new MomentDate(), false)}
+              primary={this.state.activeDataRangeButton === DATA_RANGE_LIVE}
               label="LIVE"
             />
             <RaisedButton
-              onClick={this.onDataRangeChange(DATA_RANGE_1D, (new MomentDate()).subtract(1, 'day'), new MomentDate())}
-              primary={this.state.dataRange.get('label') === DATA_RANGE_1D}
+              onClick={this.onDataRangeChange(DATA_RANGE_1D, (new MomentDate()).subtract(1, 'day'), new MomentDate(), false)}
+              primary={this.state.activeDataRangeButton === DATA_RANGE_1D}
               label="1D"
             />
             <RaisedButton
-              onClick={this.onDataRangeChange(DATA_RANGE_30D, (new MomentDate()).subtract(30, 'day'), new MomentDate())}
-              primary={this.state.dataRange.get('label') === DATA_RANGE_30D}
+              onClick={this.onDataRangeChange(DATA_RANGE_30D, (new MomentDate()).subtract(30, 'day'), new MomentDate(), false)}
+              primary={this.state.activeDataRangeButton === DATA_RANGE_30D}
               label="30D"
             />
             <RaisedButton
-              onClick={this.onDataRangeChange(DATA_RANGE_1Y, (new MomentDate()).subtract(1, 'year'), new MomentDate())}
-              primary={this.state.dataRange.get('label') === DATA_RANGE_1Y}
+              onClick={this.onDataRangeChange(DATA_RANGE_1Y, (new MomentDate()).subtract(1, 'year'), new MomentDate(), false)}
+              primary={this.state.activeDataRangeButton === DATA_RANGE_1Y}
               label="1Y"
             />
             <RaisedButton
-              onClick={this.onDataRangeChange(DATA_RANGE_CUSTOM)}
-              primary={this.state.dataRange.get('label') === DATA_RANGE_CUSTOM}
+              onClick={this.onDataRangeChange(DATA_RANGE_CUSTOM, null, null, true)}
+              primary={this.state.activeDataRangeButton === DATA_RANGE_CUSTOM}
               label="CUSTOM"
             />
           </div>
