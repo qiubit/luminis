91be818352af3195bd0d28e7c61fa378a2f4ff40
Pawel Golinski <golpaw1@gmail.com>
2017-04-26 17:14:48 +0200

    simulator: series_functions fixes

--- backend/simulator/random_series_functions.py
+++ backend/simulator/random_series_functions.py
@@ -2,22 +2,19 @@ import numpy as np
 
 from simulator.series_functions import SinSeries, ConstantSeries
 
-class RandomSinSeries(SinSeries):
-    AMPLITUDE_LOW = 1.0
-    AMPLITUDE_HIGH = 10.0
-    FREQUENCY_LOW = 0.1
-    FREQUENCY_HIGH = 2.0
 
-    def __init__(self, create_ts, update_period):
-        amplitude = np.random.uniform(self.AMPLITUDE_LOW, self.AMPLITUDE_HIGH, 1)[0]
-        frequency = np.random.uniform(self.FREQUENCY_LOW, self.FREQUENCY_HIGH, 1)[0]
+class RandomSinSeries(SinSeries):
+    def __init__(self, create_ts, update_period,
+                 amplitude_low=1.0, amplitude_high=10.0,
+                 frequency_low=0.1, frequency_high=2.0):
+        amplitude = np.random.uniform(amplitude_low, amplitude_high, 1)[0]
+        frequency = np.random.uniform(frequency_low, frequency_high, 1)[0]
         phase = np.random.uniform(0.0, 2 * np.pi, 1)[0]
         super(RandomSinSeries, self).__init__(create_ts, update_period, amplitude, frequency, phase)
 
-class RandomConstantSeries(ConstantSeries):
-    CONSTANT_LOW = -5.0
-    CONSTANT_HIGH = 5.0
 
-    def __init__(self, create_ts, update_period):
-        constant = np.random.uniform(self.CONSTANT_LOW, self.CONSTANT_HIGH, 1)[0]
+class RandomConstantSeries(ConstantSeries):
+    def __init__(self, create_ts, update_period,
+                 constant_low=-5.0, constant_high=5.0):
+        constant = np.random.uniform(constant_low, constant_high, 1)[0]
         super(RandomConstantSeries, self).__init__(create_ts, update_period, constant)
--- backend/simulator/series_functions.py
+++ backend/simulator/series_functions.py
@@ -1,17 +1,23 @@
 import numpy as np
 
-class Series():
+from abc import ABC, abstractmethod
+
+
+class Series(ABC):
     def __init__(self, create_ts, update_period):
         # timestamp of the very first data point
         self.create_ts = create_ts
         # how often new data point is being produced
         self.update_period = update_period
 
+    @abstractmethod
     def get_values(self, begin_ts, end_ts):
         pass
 
+    @abstractmethod
     def get_value(self, ts):
         pass
+
 
 class SinSeries(Series):
     def __init__(self, create_ts, update_period, amplitude, frequency, phase):
@@ -28,25 +34,26 @@ class SinSeries(Series):
             val_ts = self.amplitude * np.sin(2.0 * np.pi * self.frequency * time_from_create + self.phase)
             return val_ts
 
+    def get_value(self, ts):
+        return (ts, self.eval_sin(ts))
+
     def get_values(self, begin_ts, end_ts):
         vals = []
         for ts in np.arange(begin_ts, end_ts, self.update_period):
-            vals.append((ts, self.eval_sin(ts)))
+            vals.append(self.get_value(ts))
         return vals
 
-    def get_value(self, ts):
-        return (ts, self.eval_sin(ts))
 
 class ConstantSeries(Series):
     def __init__(self, create_ts, update_period, constant):
         self.constant = constant
         super(ConstantSeries, self).__init__(create_ts, update_period)
 
+    def get_value(self, ts):
+        return (ts, self.constant)
+
     def get_values(self, begin_ts, end_ts):
         vals = []
         for ts in np.arange(begin_ts, end_ts, self.update_period):
-            vals.append((ts, self.constant))
+            vals.append(self.get_value(ts))
         return vals
-
-    def get_value(self, ts):
-        return self.constant
