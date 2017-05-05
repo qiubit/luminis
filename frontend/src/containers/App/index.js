ba64e1823d539d004c99cbd49ed45f536abe80b5
Pawel Golinski <golpaw1@gmail.com>
2017-05-05 19:03:56 +0200

    Small UX fixes

--- frontend/src/containers/App/index.js
+++ frontend/src/containers/App/index.js
@@ -1,17 +1,15 @@
 import React from 'react';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import Drawer from 'material-ui/Drawer';
-import { ListItem, List } from 'material-ui/List';
+import { List } from 'material-ui/List';
 import darkBaseTheme from 'material-ui/styles/baseThemes/darkBaseTheme';
 import AppBar from 'material-ui/AppBar';
 import getMuiTheme from 'material-ui/styles/getMuiTheme';
 import injectTapEventPlugin from 'react-tap-event-plugin';
-import { push } from 'react-router-redux';
 import { createStructuredSelector } from 'reselect';
 import { connect } from 'react-redux';
 
 import TreeList from '../TreeList/index'
-import { MAP_URL } from '../MapPage/constants';
 import config from './config';
 import { selectDrawerOpen, selectTreeStructure } from './selectors';
 import { drawerToggle, drawerChange, changeActiveSubtree } from './actions';
@@ -39,7 +37,6 @@ class AppPage extends React.Component {
             onLeftIconButtonTouchTap={this.props.onDrawerToggle}
           />
           <List>
-            <ListItem onTouchTap={this.props.onMapOpen}>Map</ListItem>
             <TreeList/>
           </List>
         </Drawer>
@@ -59,7 +56,6 @@ function mapDispatchToProps(dispatch) {
   return {
     onRequestDrawerChange: (drawerOpen) => dispatch(drawerChange(drawerOpen)),
     onDrawerToggle: () => dispatch(drawerToggle()),
-    onMapOpen: () => dispatch(push(MAP_URL)),
     onTreeListNodeClick: (nodeId) => () => dispatch(changeActiveSubtree(nodeId)),
   };
 }
