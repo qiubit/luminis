67842ecdbf605d6f628aba0ceb6d215d5195c245
Pawel Golinski <golpaw1@gmail.com>
2017-05-07 01:54:42 +0200

    Delete unused stuff

--- frontend/src/containers/TreeList/actions.js
+++ frontend/src/containers/TreeList/actions.js
@@ -1,14 +1,6 @@
 import {
-  UPDATE_TREE_LIST_TOGGLE,
   CHANGE_NODE_TOGGLE,
 } from './constants.js'
-
-export function updateTreeListToggle(tree) {
-  return {
-    type: UPDATE_TREE_LIST_TOGGLE,
-    tree
-  }
-}
 
 export function changeNodeToggle(nodeId, nodeToggle) {
   return {
